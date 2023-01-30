0cec8c1d4c0f3f4251a86717ae16457de6dd30a2
Artem Zlyva <zlyvaartem@gmail.com>
2023-01-30 21:11:40 +0200

    synchronized 4

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1707/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task17.task1707;
+
+/* 
+synchronized 4
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        IMF fund = IMF.getFund();
+        IMF anotherFund = IMF.getFund();
+        System.out.println(fund == anotherFund);
+    }
+}
