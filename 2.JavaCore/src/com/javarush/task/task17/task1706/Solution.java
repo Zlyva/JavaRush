c4d6f6d3c785460c45f0c7253265954b08f04ddc
Artem Zlyva <zlyvaartem@gmail.com>
2023-01-30 20:58:41 +0200

    synchronized 3

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1706/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task17.task1706;
+
+/* 
+synchronized 3
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        OurPresident expectedPresident = OurPresident.getOurPresident();
+        OurPresident ourPresident = OurPresident.getOurPresident();
+        System.out.println(expectedPresident == ourPresident);
+    }
+}
