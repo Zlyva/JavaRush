1175e9d55d369002fb7ef0b467356afc02156be9
Artem Zlyva <zlyvaartem@gmail.com>
2023-02-27 10:43:54 +0200

    maxBite

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task18.task1801;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* 
+Максимальный байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        try (FileInputStream fileInputStream = new FileInputStream(new BufferedReader(new InputStreamReader(System.in)).readLine())) {
+            int maxInt = Integer.MIN_VALUE;
+            while (fileInputStream.available() > 0) {
+                int value = fileInputStream.read();
+                if (value > maxInt) {
+                    maxInt = value;
+                }
+            }
+            System.out.println(maxInt);
+        }
+    }
+}
