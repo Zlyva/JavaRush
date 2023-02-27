2708ba19a893ff514cde33c444dab8d9db60f580
Artem Zlyva <zlyvaartem@gmail.com>
2023-02-27 10:47:09 +0200

    minBite

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1802/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task18.task1802;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* 
+Минимальный байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        try (FileInputStream fileInputStream = new FileInputStream(new BufferedReader(new InputStreamReader(System.in)).readLine())) {
+            int minInt = Integer.MAX_VALUE;
+            while (fileInputStream.available() > 0) {
+                int value = fileInputStream.read();
+                if (value < minInt) {
+                    minInt = value;
+                }
+            }
+            System.out.println(minInt);
+        }
+    }
+}
